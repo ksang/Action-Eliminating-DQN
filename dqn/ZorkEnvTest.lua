6f01eac5aa610d4ae1f8eaeb449fe333e295433c
matanhs <matan.hrs@gmail.com>
2017-10-28 12:01:30 +0300

    test can show the string returned from the game

--- dqn/ZorkEnvTest.lua
+++ dqn/ZorkEnvTest.lua
@@ -2,10 +2,21 @@ require('ZorkFramework')
 opt={}
 print ()
 local gameEnv = ZorkFramework.GameEnvironment(opt)
-print ("init state vec:")
-gameEnv:getState()
-print("step 1:")
-print(gameEnv:step(1,false)) --1 > 2
-print("restarting game:\n\n\n\n\n\n\n\n\n\n\n\n\n")
-print(gameEnv:newGame())
+anomaly_count = 0
+for i=1, 1000 do
+	s,r,t,ss1 = gameEnv:newGame()
+	s,r,t,ss2 = gameEnv:step(gameEnv:getActions()[3],false) -- north
+	s,r,t,ss3 = gameEnv:step(gameEnv:getActions()[3],false) -- north
+	s,r,t,ss4 = gameEnv:step(gameEnv:getActions()[7],false) -- climb tree
+	s,r,t,ss5 = gameEnv:step(gameEnv:getActions()[8],false) -- take egg
+	s,r1,t,ss6 = gameEnv:step(gameEnv:getActions()[9],false) -- open egg
+	s,r,t,ss7 = gameEnv:step(gameEnv:getActions()[10],false) -- look
 
+	if r1 < 99 then
+	print("at iter".. i, ":",r,t)
+	print("with the folowing states:", ss1 .. ss2 .. ss3 .. ss4 .. ss5 .. ss6 .. ss7)
+	anomaly_count= anomaly_count +1
+	end
+end
+print("iteration ended after num",i)
+print("with anomaly rate",anomaly_count/i)
