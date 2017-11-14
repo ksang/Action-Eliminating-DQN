7892db60216a977101477b3f0d9031e795c6b53a
matanhs <matan.hrs@gmail.com>
2017-11-14 17:25:43 +0200

    plot now saves a small summary file which can be used directly instead of loading entire agent from disk

--- dqn/plot.lua
+++ dqn/plot.lua
@@ -3,42 +3,67 @@ require 'cunn'
 require'cutorch'
 require 'nn'
 plt = require 'gnuplot'
-lim = 600
-a1 = torch.Tensor(lim):zero()
-b1 = torch.Tensor(lim):zero()
-c1 = torch.Tensor(lim):zero()
-a2 = torch.Tensor(lim):zero()
-b2 = torch.Tensor(lim):zero()
-c2 = torch.Tensor(lim):zero()
+local agent_type = "DQN3_0_1__"
+local a_r_table = {}
+function plotAgent(agent_name,title,limit,object_net_info)
+	local agent = torch.load(agent_name..".t7")
+	--limit = math.min(#agent.reward_history,limit or #agent.reward_history)
+    print (#agent.reward_history)
+	local reward = torch.CudaTensor(limit):zero()
+	local loss = torch.CudaTensor(limit):zero()
+	local acc = torch.CudaTensor(limit):zero()
 
-
-vanila_agent = torch.load('DQN3_0_1__FULL_Y_test_vanila_zork.t7') -- zork vanila
---vanila_agent = torch.load('DQN3_0_1__FULL_Y_largereplay3.t7') -- agent with the minimal amout of actions
-
-for i=1,math.min(#vanila_agent.reward_history,lim) do
-a1[i] = vanila_agent.reward_history[i]
---print(agent.obj_loss_history[i])
---loss = vanila_agent.obj_loss_history[i]
---b1[i] = loss[1]
---c1[i] = loss[2]
+	for i=1,math.min(#agent.reward_history,limit or #agent.reward_history) do
+		reward[i] = agent.reward_history[i]
+        if object_net_info then	
+	        local temp = agent.obj_loss_history[i]
+		    loss[i] = temp[1]
+		    acc[i] = temp[2]
+        end
+	end
+    if not object_net_info then
+        loss=nil
+        acc=nil
+    end
+	
+	table.insert(a_r_table,{title or agent_name, reward:narrow(1,1,limit)})
+    --plot seperate graph for object network
+    if object_net_info then	
+        plt.figure()
+	    plt.title(title or agent_name .. "Object Network Preformance")
+	    plt.plot({'Binary Cross Entropy loss', loss:narrow(1,1,limit)},{'Accuracy',acc:narrow(1,1,limit)})
+	    plt.movelegend('right','bottom')
+	    plt.xlabel('Epochs, 10000 steps per epoch')
+    end
+    torch.save(agent_name.."_result_summary.t7",{reward = reward,loss = loss,acc = acc,limit =limit, title =title or agent_name})
 end
-vanila_agent = nil
-
-merged_agent = torch.load('DQN3_0_1__FULL_Y_test_obj_net_exploration_n_greedy_restriction_reval.t7')
-for i=1,math.min(lim,#merged_agent.reward_history) do
-a2[i] = merged_agent.reward_history[i]
---print(agent.obj_loss_history[i])
-loss = merged_agent.obj_loss_history[i]
-b2[i] = loss[1]
-c2[i] = loss[2]
+function plotAgentFromSummary(agent_name)
+    agent_summary = torch.load(agent_name.."_result_summary.t7")
+    if agent_summary.loss then	
+        plt.figure()
+	    plt.title(agent_summary.title .. "Object Network Preformance")
+	    plt.plot({'Binary Cross Entropy loss', agent_summary.loss:narrow(1,1,agent_summary.limit)},{'Accuracy',agent_summary.acc:narrow(1,1,agent_summary.limit)})
+	    plt.movelegend('right','bottom')
+	    plt.xlabel('Epochs, 10000 steps per epoch')
+    end
+	table.insert(a_r_table,{agent_summary.title, agent_summary.reward:narrow(1,1,agent_summary.limit)})
 end
+
+local limit=155
+
+plt.figure(1)
+--insert agents here
+
+--plotAgent(agent_type.."FC_restrict_exploration_n_action", "Merged Agent", limit,true)
+--plotAgent(agent_type.."FC_restrict_exploration", "Restricted exploration", limit,true)
+plotAgent("DQN3_0_1__zork_FC_vanila_scenario_2_lr_1.9e7","vanila extended action space",limit,false)
+--plotAgentFromSummary("_zork_FC_vanila_scenario_2")
+plotAgent("DQN3_0_1__zork_FC_merged_scenario_2_lr_1.9e7","merged extended action space",limit,true)
+--plotAgentFromSummary("_zork_FC_merged_scenario_2")
+plotAgent("DQN3_0_1_zork_FC_greedy_scenario_2_lr1.7e7","greedy extended action space",limit,true)
+--plot in main reward graph
 plt.figure(1)
 plt.title('DQN Agent Reward')
-plt.xlabel('epochs')
-plt.plot({'Vanila Zork', a1:narrow(1,1,lim)},{'Action and Exploration Restriction', a2:narrow(1,1,lim)})
+plt.xlabel('Epochs, 10000 steps per epoch')
 plt.movelegend('right','bottom')
-plt.figure(2)
-plt.plot({'Binary Cross Entropy loss', b2:narrow(1,1,lim)},{'Accuracy',c2:narrow(1,1,lim)})
-plt.movelegend('right','bottom')
-plt.title('Object Conv Net preformance')
-plt.xlabel('epochs')
+plt.plot(a_r_table)	
