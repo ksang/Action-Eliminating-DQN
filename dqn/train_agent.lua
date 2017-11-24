1ea180d7831f2ea1551a48d17e6a23a77e3eab74
matanhs <matan.hrs@gmail.com>
2017-11-25 00:45:42 +0200

    rewrite agent file name convention

--- dqn/train_agent.lua
+++ dqn/train_agent.lua
@@ -200,8 +200,8 @@ while step < opt.steps do
         if opt.save_versions > 0 then
             filename = filename .. "_" .. math.floor(step / opt.save_versions)
         end
-        filename = filename
-        torch.save(filename .. ".t7", {agent = agent,
+        filename = filename  .. "_lr" .. agent.lr .."_" ..agent.n_actions.."a"
+        torch.save(filename ..".t7", {agent = agent,
                                 model = agent.network,
                                 best_model = agent.best_network,
                                 reward_history = reward_history,
